b38dd54cbaaaa9daa452d3b70c95ece802f7471e
Nguyễn Huyền Trang <125456509+HuyenTrang0506@users.noreply.github.com>
2025-04-21 02:08:55 +0700

    setup resilience4j for protect global scope of project

--- xxxx-controller/src/main/java/com/xxxx/ddd/controller/resource/HiController.java
+++ xxxx-controller/src/main/java/com/xxxx/ddd/controller/resource/HiController.java
@@ -1,6 +1,7 @@
 package com.xxxx.ddd.controller.resource;
 
 import com.xxxx.application.service.event.EventAppService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +13,18 @@ public class HiController {
     @Autowired
     private EventAppService eventAppService;
     @GetMapping("/hi")
+    @RateLimiter(name="backendA", fallbackMethod = "fallbackHello")
     public String hello() {
 //        return "Hello, World!";
-        return eventAppService.sayHi("Trang");
+        return eventAppService.sayHi("Hi");
+    }
+    public String fallbackHello(Throwable throwable) {
+        return "Too many requests, please try again later.";
+    }
+    @GetMapping("/hi/v1")
+    @RateLimiter(name="backendB", fallbackMethod = "fallbackHello")
+    public String sayHi() {
+//        return "Hello, World!";
+        return eventAppService.sayHi("Ho");
     }
 }
